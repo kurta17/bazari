6ed5a8c8dc0933fbbcfac6d4bd8adc266c1caa88
kere199 <kereleishviliirakli81@gmail.com>
2024-05-20 10:36:38 +0400

    creating app python file

---
+++ app.py
@@ -0,0 +1,9 @@
+from flask import Flask
+from flask import Flask, flash, redirect, render_template, request, session, url_for
+from flask_login import LoginManager
+
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = 'mysecret'
+login_manager = LoginManager()
+
