b14eca906251ab61011cf5c5d7c9bdbda8817429
kere199 <kereleishviliirakli81@gmail.com>
2024-05-20 11:00:19 +0400

    working with blueprint

---
+++ views.py
@@ -0,0 +1,11 @@
+from flask import Flask, Blueprint
+from flask import Flask, flash, redirect, render_template, request, session, url_for
+from flask_login import LoginManager
+
+
+bp = Blueprint('users',__name__,template_folder='templates')
+
+@bp.route('/login')
+def login():
+    return render_template('login.html')
+
