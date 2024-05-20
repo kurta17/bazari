e2aaf67c2fab5b1de8323728767e8a683f8d5a32
kurta17 <114169244+kurta17@users.noreply.github.com>
2024-05-20 14:17:09 +0700

    register and layout

--- views.py
+++ views.py
@@ -9,3 +9,7 @@ bp = Blueprint('users',__name__,template_folder='templates')
 def login():
     return render_template('login.html')
 
+
+@bp.route('/register')
+def register():
+    return render_template('register.html')
