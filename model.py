25d7daed31804d8ad50476d070f33f7cc628e2d2
kurta17 <114169244+kurta17@users.noreply.github.com>
2024-05-22 16:16:52 +0700

    working with data model

--- model.py
+++ model.py
@@ -1,23 +1,79 @@
-import datetime
+import json
+from datetime import datetime
 
 class Message():
-    def __init__(self , text: str, author:str, created: datetime ):
-        self.text : str = text
-        self.author : str = author
-        self.created : datetime = created
-        self.group = 'Group'
-
+    def __init__(self, text: str, author: str, created: datetime, group: str):
+        self.text = text
+        self.author = author
+        self.created = created
+        self.group = group
 
     def as_json(self):
-        return {"author": self.author, 'text': self.text, 'time': self.created }
+        return {
+            "author": self.author,
+            "text": self.text,
+            "created": self.created.isoformat(),
+            "group": self.group
+        }
+    
+    @staticmethod
+    def from_json(data):
+        return Message(
+            text=data['text'],
+            author=data['author'],
+            created=datetime.fromisoformat(data['created']),
+            group=data['group']
+        )
 
 
 class Group():
-    def __init__(self, messages:list):
-        self.messages: list = messages 
+    def __init__(self, name: str,  admin : str , messages: list = None):
+        self.name = name
+        self.admin = admin
+        self.messages = messages if messages else []
 
     def as_json(self):
-        return [message for message in self.messages]
-    
+        return {
+            "name": self.name,
+            "admin": self.admin,
+            "messages": [message.as_json() for message in self.messages]
+        }
+
+    @staticmethod
+    def from_json(data):
+        messages = [Message.from_json(msg) for msg in data['messages']]
+        return Group(name=data['name'], admin=data['admin'], messages=messages)
 
 
+class MessageBoard():
+    def __init__(self, groups: list = None):
+        self.groups = groups if groups else []
+
+    def add_group(self, group: Group):
+        self.groups.append(group)
+
+    def save_to_file(self, filename: str):
+        with open(filename, 'w') as f:
+            json.dump([group.as_json() for group in self.groups], f, indent=4)
+
+    @staticmethod
+    def load_from_file(filename: str):
+        with open(filename, 'r') as f:
+            groups_data = json.load(f)
+            groups = [Group.from_json(group_data) for group_data in groups_data]
+            return MessageBoard(groups=groups)
+
+
+# # Example usage
+# new_message = Message('Hello', 'John', datetime.utcnow(), 'Group1')
+# new_group = Group(name='Group1',admin='keree', messages=[new_message])
+# board = MessageBoard(groups=[new_group])
+
+# # Save to file
+# board.save_to_file('message_board.json')
+
+# # Load from file
+# loaded_board = MessageBoard.load_from_file('message_board.json')
+# add_message_to_group(loaded_board, 'Group1', Message('Another message', 'Alice', datetime.utcnow(), 'Group1'))
+
+# print(loaded_board.groups[0].messages[1].text)  
--- views.py
+++ views.py
@@ -1,4 +1,5 @@
 # views.py
+from datetime import datetime
 from flask import Blueprint, flash, redirect, render_template, request, url_for
 from flask_login import LoginManager, UserMixin, login_user, logout_user
 from wtforms import StringField, PasswordField, SubmitField
@@ -7,15 +8,21 @@ from flask_login import login_manager
 from user import User, UserRegistration
 import json
 from flask_login import login_required
+from model import Group,MessageBoard,Message, add_message_to_group
 
-groups = {'python': {'creator':'kere'}}
+
+
+message1 = Message('Hello', 'Kurta', datetime.now(), 'python')
+message2 = Message('Hi', 'Kurta', datetime.now(), 'java')
+group1 = Group('python',  'kere',  [message1])
+group2 = Group('java', 'kere', [message2, message1])
+initial_groups = MessageBoard([group1, group2])
 
 bp = Blueprint('users', __name__, template_folder='templates')
 
 login_manager = LoginManager()
 
 initial_users = {'kurta@gmail.com': {'password': '1234', 'name': 'Kurta'}}
-
 
 def load_users_from_file():
     try:
@@ -66,8 +73,29 @@ def logout():
 @bp.route('/groups', methods=['GET', 'POST'])
 @login_required
 def groups():
-    return render_template('groups.html')
+    groups = initial_groups.groups
+    return render_template('groups.html', groups=groups)
 
+@bp.route('/group/<group_name>')
+@login_required
+def group_detail(group_name):
+    group = next((group for group in initial_groups.groups if group.name == group_name), None)
+    if group is None:   
+        return redirect(url_for('users.groups'))
+    return render_template('group_detail.html', group=group)
+
+
+@bp.route('/add_message', methods=['GET', 'POST'])
+@login_required
+def add_message():
+    if request.method == 'POST':
+        text = request.form['text']
+        group_name = request.form['group']
+        author = request.form['author']
+        message = Message(text, author, datetime.now(), group_name)
+        add_message_to_group(initial_groups, group_name, message)
+        return redirect(url_for('users.group_detail', group_name=group_name))
+    return render_template('add_message.html', groups=initial_groups.groups)
 
 @bp.route('/add_group', methods=['GET', 'POST'])
 @login_required
