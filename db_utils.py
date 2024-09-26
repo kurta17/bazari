28343608afc822e51e1d7a904057b8df7bf9c8e7
Giorgi Kurtanidze <thinkers.struggle@gmail.com>
2024-09-27 00:18:04 +0700

    wp

--- db_utils.py
+++ db_utils.py
@@ -5,7 +5,7 @@ import os
 POSTGRES_USER = os.getenv('POSTGRES_USER', 'postgres')
 POSTGRES_PASSWORD = os.getenv('POSTGRES_PASSWORD', 'postgres')
 POSTGRES_DB = os.getenv('POSTGRES_DB', 'postgres')
-POSTGRES_HOST = os.getenv('POSTGRES_HOST', 'localhost')  # Or 'postgres' if using Docker compose
+POSTGRES_HOST = os.getenv('POSTGRES_HOST', 'postgres')  # Or 'postgres' if using Docker compose
 POSTGRES_PORT = os.getenv('POSTGRES_PORT', '5432')
 
 def get_db_connection():
